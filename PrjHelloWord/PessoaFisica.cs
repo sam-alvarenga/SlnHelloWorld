f4193676356893056e61eed98a0a9696008fef5d
Samantha <samantha.c.alvarenga@gmail.com>
2024-10-10 02:16:11 -0300

    documentando passo a passo do código para gerar o nuget

--- PrjHelloWord/PessoaFisica.cs
+++ PrjHelloWord/PessoaFisica.cs
@@ -8,11 +8,18 @@ using System.Threading.Tasks;
 
 namespace SamAlvarenga.PrjHelloWorld.Models
 {
+
+    /// <summary>
+    /// Representa uma pessoa física no sistema, contendo além de nome e idade, o cpf.
+    /// </summary>
     public class PessoaFisica : Pessoa
     {
         //encapsulamento do cpf
         private string _cpf;
 
+        /// <summary>
+        /// Representa o cpf da pessoa física. Valores que não possuam 11 caracteres não serão aceitos.
+        /// </summary>
         public string Cpf
         {
             get { return _cpf; }
@@ -26,7 +33,12 @@ namespace SamAlvarenga.PrjHelloWorld.Models
             }
         }
 
-
+        /// <summary>
+        /// Representação de uma pessoa física
+        /// </summary>
+        /// <param name="pNome">Representa o nome da pessoa física. Valores nulos ou com menos de dois caracteres não são admitidos</param>
+        /// <param name="pIdade">Representa a idade da pessoa física. Valores menores que 1 e maiores que 135 não sao admitidos.</param>
+        /// <param name="pCpf">Representa o número de cadastro da pessoa física. Valores com quantidade de caracteres diferente de 11 não serão aceitos</param>
         public PessoaFisica(string pNome, int pIdade, string pCpf) : base (pNome,pIdade) 
         {
             this.Cpf = pCpf;
--- PrjHelloWord/PessoaJuridica.cs
+++ PrjHelloWord/PessoaJuridica.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace SamAlvarenga.PrjHelloWorld.Models
 {
+    /// <summary>
+    /// Representa uma pessoa jurídica.
+    /// Herda da classe <see cref="Pessoa"/>.
+    /// </summary>
     public class PessoaJuridica : Pessoa
     {
         private string _Cnpj ;
 
+        /// <summary>
+        /// Obtém ou define o CNPJ da pessoa jurídica.
+        /// Lança uma exceção se o CNPJ não tiver 15 caracteres.
+        /// </summary>
+        /// <exception cref="Exception">Lançada quando o CNPJ é inválido.</exception>
         public string Cnpj
         {
             get { return _Cnpj; }
@@ -30,6 +39,12 @@ namespace SamAlvarenga.PrjHelloWorld.Models
 
 
         // Construtor para inicializar os dados da PessoaJuridica
+        /// <summary>
+        ///  Representação de uma pessoa juridica
+        /// </summary>
+        /// <param name="pNome">Representa o nome da pessoa jurídica. Valores nulos ou com menos de dois caracteres não são admitidos.</param>
+        /// <param name="pIdade">Representa a idade da pessoa jurídica. Valores menores que 1 e maiores que 135 não sao admitidos.</param>
+        /// <param name="pjCnpj">Representa o CNPJ da pessoa jurídica. Valores com quantidade de caracteres diferente de 15 não serão aceitos.</param>
         public PessoaJuridica(string pNome, int pIdade, string pjCnpj) : base(pNome, pIdade)
         {
           
